b3e79dbc7538de78225dc5c98acf5f6dcae446d0
arifhossain123 <arifhossain42136@gmail.com>
2020-07-13 21:07:39 +0600

    Updates

---
+++ precache-manifest.2cefc8951c2fe59c1b9add44c67e6a37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e8ef607ba5d1e9d781d1574a99e44b",
+    "url": "/Flagship_Mobiles-2020/index.html"
+  },
+  {
+    "revision": "52e3fcd83282171b992e",
+    "url": "/Flagship_Mobiles-2020/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9187e27ddb3bbfb66c76",
+    "url": "/Flagship_Mobiles-2020/static/css/main.ac7bf558.chunk.css"
+  },
+  {
+    "revision": "52e3fcd83282171b992e",
+    "url": "/Flagship_Mobiles-2020/static/js/2.cc0d7a8d.chunk.js"
+  },
+  {
+    "revision": "292c0dcab4b2aa797006a497ac447956",
+    "url": "/Flagship_Mobiles-2020/static/js/2.cc0d7a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9187e27ddb3bbfb66c76",
+    "url": "/Flagship_Mobiles-2020/static/js/main.29c85324.chunk.js"
+  },
+  {
+    "revision": "7f4cdbb7732e674882ec",
+    "url": "/Flagship_Mobiles-2020/static/js/runtime-main.2385b39e.js"
+  }
+]);
